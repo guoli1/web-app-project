ae8518e0b39d3f8e3834721b903270c03f388408
LISHAN GUO <lishanguo@LISHANs-MacBook-Pro.local>
2020-06-01 17:56:26 -0400

    aws-eb-docker-nginx-proxy-original:fix:update build_zip_artifacts scripts to create the CF and Elasticbeanstalk version zip files

--- deployment_scripts/build_zip_artifacts.sh
+++ deployment_scripts/build_zip_artifacts.sh
@@ -7,24 +7,29 @@ set -e
 set +x
 
 # example command to run script:
-#sh deployment_scripts/build_zip_artifact.sh -f eb-docker-nginx-proxy.zip -p eb-docker-nginx-proxy
+#sh deployment_scripts/build_zip_artifacts.sh -c cf-bundle.zip -f eb-docker-nginx-proxy.zip -p eb-docker-nginx-proxy
 
 usage(){
     echo "Usage: "
-    echo "Build the elasticbeanstalk application version zip artifact. Usage:
-$(basename "$0") [-h] [-f -p]
+    echo "Build the elasticbeanstalk application version and cloudformation files zip artifacts. Usage:
+$(basename "$0") [-h] [-c -f -p]
 Where:
     -h: Show this usage message.
+    -c: CloudFormation template and configuration file zip artifact file name. (e.g. cf-bundle.zip)
     -f: Elasticbeanstalk application version zip artifact file name. (eg. eb-docker-nginx-proxy.zip)
     -p: Project folder name. (e.g. eb-docker-nginx-proxy)
+
 "
 }
 
-while getopts "hf:p:" opt; do
+while getopts "hc:f:p:" opt; do
     case ${opt} in
         h )
             usage
             exit 0
+          ;;
+        c )
+            CLOUDFORMATION_FILE_NAME=$OPTARG
           ;;
         f )
             FILE_NAME=$OPTARG
@@ -45,6 +50,13 @@ while getopts "hf:p:" opt; do
     esac
 done
 
+
+if [ -z "$CLOUDFORMATION_FILE_NAME" ]; then
+  echo "Cloudformation template and configuration zip file name is a required argument."
+  usage
+  exit 1
+fi
+
 if [ -z "$FILE_NAME" ]; then
   echo "Elasticbeanstalk application version zip file name is a required argument."
   usage
@@ -56,15 +68,22 @@ if [ -z "$PROJECT_NAME" ]; then
   exit 1
 fi
 
-dist_folder_name=dist
+target_folder_name=target
 
-# Create source code zip file
+# Create target directory
+rm -rf $target_folder_name
+mkdir $target_folder_name
+
+# Create elasticbeanstalk version source code zip file
 cd $PROJECT_NAME
-rm -rf $dist_folder_name
-mkdir $dist_folder_name
-zip -r $dist_folder_name/$FILE_NAME . -x "*dist*" -x ".DS_Store" -x "__MACOSX"
+zip -r ../$target_folder_name/$FILE_NAME . -x ".DS_Store" -x "__MACOSX"
 cd ..
 
-# List files under $PROJECT_NAME/$dist_folder_name/ folder
-echo `pwd`"/$PROJECT_NAME/$dist_folder_name/ folder contains:"
-ls -la $PROJECT_NAME/$dist_folder_name
+# Create cloudformation template and configuration zip file
+cd cf_templates
+zip -r ../$target_folder_name/$CLOUDFORMATION_FILE_NAME . -x ".DS_Store" -x "__MACOSX"
+cd ..
+
+# List files under $target_folder_name/ folder
+echo `pwd`"/$target_folder_name/ folder contains:"
+ls -la $target_folder_name
